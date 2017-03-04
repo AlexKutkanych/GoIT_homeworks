d63e219052656487ba8e4d3fcdd16193e2b4364b
AlexK <alexander.kutkanych@gmail.com>
2017-03-04 11:28:55 +0200

    our services with animated cover block

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-$(function(){
-  var $newDiv = $('.sliding-block');
-  console.log($newDiv);
-  var $pic = $('.serv-pic1');
-  $pic.on('click', function(){
-    // $newDiv.css('display', 'block');
-    alert('block');
-  });
 
-  var $active = $('.menu-item');
-  // console.log($active);
-  $active.on('click', function(){
-    $active.addClass('active').siblings().removeClass('active');
-  });
+// $(function(){
+//   var $block1 = $('.block1');
+//
+//   var $pic1 = $('.serv-pic1');
+//   var $ancor = $('.service1 a');
+//   console.log($pic1);
+//     $pic1.mouseenter(function(){
+//       $block1.addClass("active-block");
+//       // $block1.animate({"bottom":"+=260px"});
+//       $ancor.addClass("yellow");
+//     });
+//
+//     $pic1.mouseleave(function(){
+//       $block1.removeClass("active-block");
+//       // $block1.stop().animate({"bottom":"0"});
+//       $ancor.removeClass("yellow");
+//     });
+
+//   $pic1.mouseenter(function(){
+//     // $block1.toggleClass("active-block");
+//       $block1.animate({"bottom":"+=260px"});
+//     $ancor.addClass("yellow");
+//   });
+// });
+// $pic1.mouseleave(function(){
+//       $block1.css({"top": "260px"});
+//       // $block1.animate({"bottom": "-=260px"});
+//       $ancor.removeClass("yellow");
+//     });
 
 });
